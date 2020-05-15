36f6a7e8440e40415bc1b5902f184ac85e5b65f1
vpasport <pasportnikov2000@gmail.com>
2020-05-16 00:54:05 +0800

    added user

--- src/main/java/ru/isu/CourseProject/model/User.java
+++ src/main/java/ru/isu/CourseProject/model/User.java
@@ -1,4 +1,16 @@
 package ru.isu.CourseProject.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String secondName;
+    private String lastName;
+    private String nickname;
 }
